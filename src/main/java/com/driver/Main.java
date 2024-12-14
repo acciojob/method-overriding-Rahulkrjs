87d243883a6ea8210fd873d61231087aa2b2549a
Rahul Kumar Jaiswal <rahulkumar8533205@gmail.com>
2024-12-14 22:40:33 +0530

    METHOD OVRRIDE

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -21,11 +21,11 @@ public class Main {
     public static void main(String[] args) {
         B obj1 = new B();
 
-        System.out.println(obj1.meth());
+        obj1.meth();
 
         B obj2 = new B();
 
-        System.out.println(obj2.meth());
+        obj2.meth();
     }
 
 }
