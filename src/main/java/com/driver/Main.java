15492a87cff17a9d0360c3359de4e9b11fe515cb
Rahul Kumar Jaiswal <rahulkumar8533205@gmail.com>
2024-12-14 22:19:17 +0530

    Override Method

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,31 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static class A{
+        public String math(){
+
+            return "Invoking method from class A";
+
+        }
+    }
+
+    public static class B extends A {
+        @Override
+        public String math() {
+            return "Method is overridden in Extended class B";
+        }
+
+    }
+
+    public static void main(String[] args) {
+        B obj1 = new B();
+
+        System.out.println(obj1.math());
+
+        B obj2 = new B();
+
+        System.out.println(obj2.math());
+    }
+
 }
